d9148c83f17fef124a2d5c35ac5d4e899db25950
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-11-08 22:05:04 +0100

    Updated module-info with comments

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,36 @@
+/* 
+ * Define a new module named 'javafx.gui.lab'. 
+ * This is the name of the module and can be used by 
+ * other modules to refer to this one.
+ */
 module javafx.gui.lab {
-    exports se.lu.ics.models;
-    exports se.lu.ics;
 
-    requires javafx.base;
+    /* 
+     * Export the 'se.lu.ics' package, allowing other modules
+     * to access the public classes and interfaces within this package.
+     */
+    exports se.lu.ics;
+    exports se.lu.ics.controllers;
+
+    /* 
+     * Declare that this module requires the 'javafx.fxml' module to function. 
+     * This means that the current module depends on 
+     * the JavaFX FXML library for its functionality.
+     */
     requires javafx.fxml;
-    requires javafx.graphics;
+
+    /* 
+     * Declare that this module requires the 'javafx.controls' module. 
+     * This means it uses the JavaFX Controls library, which includes 
+     * UI components like buttons, text fields, etc.
+     */
+    requires javafx.controls;
+
+    /* 
+     * Declare a transitive dependency on the 'javafx.graphics' module. 
+     * This means that not only does this module 
+     * require 'javafx.graphics', but any module that requires 
+     * this module ('javafx.gui.lab') will also implicitly require 'javafx.graphics'.
+     */
+    requires transitive javafx.graphics;
 }
