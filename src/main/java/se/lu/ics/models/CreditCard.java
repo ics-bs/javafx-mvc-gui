e92e7ddc6e542297e180f1fb33e11388881a0dbd
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-11-08 09:17:42 +0100

    Implemented CreditCard class

---
+++ src/main/java/se/lu/ics/models/CreditCard.java
@@ -0,0 +1,27 @@
+package se.lu.ics.models;
+
+public class CreditCard {
+    private String number;
+    private String type;
+
+    public CreditCard(String number, String type) {
+        this.number = number;
+        this.type = type;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }  
+}
